f5bd3188f1184688e4299174d1910d0878690afa
Robert Watson <spartan83@gmail.com>
2018-04-14 23:47:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
 #select titles from projects select plege ammounts from pledges sort by name
-SELECT title, amount FROM projects INNER JOIN pledges ON project_ID=project_ID;
+SELECT projects.title, pledges.amount FROM (projects INNER JOIN pledges ON pledges.project_ID=projects.ID);
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
