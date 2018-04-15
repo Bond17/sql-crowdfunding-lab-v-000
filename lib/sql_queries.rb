b8e0f09e22f52bc98468fa0d42202a42b3501f12
Robert Watson <spartan83@gmail.com>
2018-04-15 00:16:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,12 +17,13 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "Write your SQL query Here"
 "SELECT users.name, users.age, sum(pledges.amount)
-FROM users INNER JOIN pledges ON pledges.user_id=users.id GROUP BY name"
+FROM users INNER JOIN pledges ON pledges.user_id=users.id GROUP BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "Write your SQL query Here"
-""
+"SELECT projects.title, sum(pledges.amount)-projects.funding_goal
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
